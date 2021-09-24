492d4508d0046b74fbc0a2b93870ff746daa47db
Mike DeVita <mike@relative.media>
2021-09-24 15:58:26 -0700

    bump

--- resources/js/Layouts/Guest.js
+++ resources/js/Layouts/Guest.js
@@ -11,6 +11,7 @@ export default function Guest({ appTitle, pageTitle, children }) {
     return (<div id='GuestLayout' className={classNames('layout', 'guest-layout')}>
         <Helmet>
             <title>{title}</title>
+            <meta name='description' content='VAMS is a web bassed Virtual Airline Management System that synchronizes with OnAir Company and can import data from NeoFly.' />
         </Helmet>
         {children}
     </div>);
